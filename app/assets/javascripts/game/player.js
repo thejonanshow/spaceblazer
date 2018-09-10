42c07ed168cd75d788d50d04ab794609e31b063b
Jonan Scheffler <jonanscheffler@gmail.com>
2018-09-10 14:04:48 -0700

    Bullets destroy enemies.

--- app/assets/javascripts/game/player.js
+++ app/assets/javascripts/game/player.js
@@ -56,8 +56,14 @@ class Player {
 
   fire() {
     let bullet = bullets.create(this.sprite.x + 50, this.sprite.y + 20, this.bullet + '1');
+    scene.physics.add.collider(bullet, enemies, this.bullet_strike, null, scene);
     bullet.play(this.bullet);
     bullet.setVelocityX(Player.bullets[this.bullet].speed);
+  };
+
+  bullet_strike(bullet, enemy) {
+    enemy.destroy();
+    bullet.destroy();
   };
 };
 
