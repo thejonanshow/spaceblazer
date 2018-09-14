6c6d5ad3d5721f984d27089e78fd60b4d88610f1
Jonan Scheffler <jonanscheffler@gmail.com>
2018-09-13 19:16:55 -0700

    Simulator can toggle unique IDs.

--- command_simulator.rb
+++ command_simulator.rb
@@ -11,8 +11,21 @@ command = nil
 
 ids = (0..1000).to_a
 
+id_mode = true
 while command != 'q'
-  puts "Enter a command or (q)uit:"
+  if id_mode
+    puts "IDs are unique"
+  else
+    puts "IDs are NOT unique"
+  end
+
+  puts "Enter a command (toggle id (m)ode, (q)uit):"
   command = gets.chomp
-  redis.publish(ENV['REDIS_CHANNEL'], { id: "simulator#{ids.shift}", command: command }.to_json)
+
+  if command == 'm'
+    id_mode = !id_mode
+    next
+  end
+
+  redis.publish(ENV['REDIS_CHANNEL'], { id: "simulator#{id_mode ? ids.shift : ''}", command: command }.to_json)
 end
