81b9ff30ece75c80f82e65a3087b600eff240e51
Chris Castle <castle@heroku.com>
2018-09-15 16:41:08 -0700

    Allow two players to use one keyboard

--- app/assets/javascripts/game/keyboard.js
+++ app/assets/javascripts/game/keyboard.js
@@ -1,14 +1,14 @@
 //= require ./fingerprint
 
-KEYDOWN_EVENTS = {
-  A: "a",
-  B: "b",
-  X: "x",
-  Y: "y",
-  TAB: "t",
+KEYDOWN_EVENTS_PLAYER_1 = {
+  K: "a",
+  L: "b",
+  COMMA: "x",
+  PERIOD: "y",
+  BACKSLASH: "t",
   ENTER: "s",
-  O: "o",
-  W: "w",
+  OPEN_BRACKET: "o",
+  CLOSE_BRACKET: "w",
   UP: "u",
   DOWN: "d",
   LEFT: "l",
@@ -16,15 +16,15 @@ KEYDOWN_EVENTS = {
   SPACE: "b"
 };
 
-KEYUP_EVENTS = {
-  A: "1",
-  B: "2",
-  X: "3",
-  Y: "4",
-  TAB: "5",
+KEYUP_EVENTS_PLAYER_1 = {
+  K: "1",
+  L: "2",
+  COMMA: "3",
+  PERIOD: "4",
+  BACKSLASH: "5",
   ENTER: "6",
-  O: "7",
-  W: "8",
+  OPEN_BRACKET: "7",
+  CLOSE_BRACKET: "8",
   UP: "9",
   DOWN: "0",
   LEFT: "-",
@@ -32,29 +32,88 @@ KEYUP_EVENTS = {
   SPACE: "2"
 };
 
+KEYDOWN_EVENTS_PLAYER_2 = {
+  R: "a",
+  T: "b",
+  F: "x",
+  G: "y",
+  BACKTICK: "t",
+  TAB: "s",
+  NUMBER_ONE: "o",
+  NUMBER_TWO: "w",
+  W: "u",
+  S: "d",
+  A: "l",
+  D: "r",
+  X: "b"
+};
+
+KEYUP_EVENTS_PLAYER_2 = {
+  R: "1",
+  T: "2",
+  F: "3",
+  G: "4",
+  BACKTICK: "5",
+  TAB: "6",
+  NUMBER_ONE: "7",
+  NUMBER_TWO: "8",
+  W: "9",
+  S: "0",
+  A: "-",
+  D: "=",
+  X: "2"
+};
+
 function add_keyboard_controls(scene) {
-  Object.keys(KEYDOWN_EVENTS).forEach(function(keyname) {
+  Object.keys(KEYDOWN_EVENTS_PLAYER_1).forEach(function(keyname) {
     scene.input.keyboard.on('keydown_' + keyname, function (event) {
       console.log(keyname);
-      if ((keyname == 'ENTER') || (Player.active_players[fingerprint])) {
+      if ((keyname == 'ENTER') || (Player.active_players[fingerprint + "player1"])) {
         App.cable.subscriptions.subscriptions[0].perform(
           "echo_command",
           {
-            id: fingerprint,
-            command: KEYDOWN_EVENTS[keyname]
+            id: fingerprint + "player1",
+            command: KEYDOWN_EVENTS_PLAYER_1[keyname]
           }
         );
       }
     });
   });
-  Object.keys(KEYUP_EVENTS).forEach(function(keyname) {
+  Object.keys(KEYUP_EVENTS_PLAYER_1).forEach(function(keyname) {
     scene.input.keyboard.on('keyup_' + keyname, function (event) {
-      if (Player.active_players[fingerprint]) {
+      if (Player.active_players[fingerprint + "player1"]) {
         App.cable.subscriptions.subscriptions[0].perform(
           "echo_command",
           {
-            id: fingerprint,
-            command: KEYUP_EVENTS[keyname]
+            id: fingerprint + "player1",
+            command: KEYUP_EVENTS_PLAYER_1[keyname]
+          }
+        );
+      }
+    });
+  });
+  Object.keys(KEYDOWN_EVENTS_PLAYER_2).forEach(function(keyname) {
+    scene.input.keyboard.on('keydown_' + keyname, function (event) {
+      console.log(keyname);
+      if ((keyname == 'TAB') || (Player.active_players[fingerprint + "player2"])) {
+        App.cable.subscriptions.subscriptions[0].perform(
+          "echo_command",
+          {
+            id: fingerprint + "player2",
+            command: KEYDOWN_EVENTS_PLAYER_2[keyname]
+          }
+        );
+      }
+    });
+  });
+  Object.keys(KEYUP_EVENTS_PLAYER_2).forEach(function(keyname) {
+    scene.input.keyboard.on('keyup_' + keyname, function (event) {
+      if (Player.active_players[fingerprint + "player2"]) {
+        App.cable.subscriptions.subscriptions[0].perform(
+          "echo_command",
+          {
+            id: fingerprint + "player2",
+            command: KEYUP_EVENTS_PLAYER_2[keyname]
           }
         );
       }
