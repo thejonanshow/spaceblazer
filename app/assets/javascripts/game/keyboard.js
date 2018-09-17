fb4e0e3dfa13c352de573fd0e01ab702112f1774
Jonan Scheffler <jonanscheffler@gmail.com>
2018-09-17 12:59:43 -0700

    Take out second keyboard player.

--- app/assets/javascripts/game/keyboard.js
+++ app/assets/javascripts/game/keyboard.js
@@ -1,4 +1,4 @@
-KEYDOWN1 = {
+KEYDOWN = {
   K: "a",
   L: "b",
   COMMA: "x",
@@ -14,7 +14,7 @@ KEYDOWN1 = {
   SPACE: "b"
 };
 
-KEYUP1 = {
+KEYUP = {
   K: "1",
   L: "2",
   COMMA: "3",
@@ -30,38 +30,6 @@ KEYUP1 = {
   SPACE: "2"
 };
 
-KEYDOWN2 = {
-  R: "a",
-  T: "b",
-  F: "x",
-  G: "y",
-  BACKTICK: "t",
-  V: "s",
-  NUMBER_ONE: "o",
-  NUMBER_TWO: "w",
-  W: "u",
-  S: "d",
-  A: "l",
-  D: "r",
-  X: "b"
-};
-
-KEYUP2 = {
-  R: "1",
-  T: "2",
-  F: "3",
-  G: "4",
-  BACKTICK: "5",
-  V: "6",
-  NUMBER_ONE: "7",
-  NUMBER_TWO: "8",
-  W: "9",
-  S: "0",
-  A: "-",
-  D: "=",
-  X: "2"
-};
-
 function echo_command(id, command) {
   App.cable.subscriptions.subscriptions[0].perform(
     "echo_command", { id: id, command: command }
@@ -72,29 +40,28 @@ function addKeyboardControls(scene) {
   scene.input.keyboard.on('keydown', function(event) {
     debugLog(event);
   });
+  scene.input.keyboard.on('keyup', function(event) {
+    debugLog(event);
+  });
 
-  [KEYDOWN1, KEYDOWN2].forEach(function(keyMap, index) {
-    Object.keys(keyMap).forEach(function(keyname) {
-      scene.input.keyboard.on('keydown_' + keyname, function (event) {
-        let id = game.fingerprint + 'player' + (index + 1);
+  Object.keys(KEYUP).forEach(function(keyname) {
+    scene.input.keyboard.on('keydown_' + keyname, function (event) {
+      let id = game.fingerprint;
 
-        if ((keyname == 'ENTER') || (keyname == 'V') || (Player.activePlayers[id])) {
-          command = keyMap[keyname]
-          echo_command(id, command);
-        }
-      });
+      if ((keyname == 'ENTER') || (Player.activePlayers[id])) {
+        command = KEYDOWN[keyname]
+        echo_command(id, command);
+      }
     });
   });
-  [KEYUP1, KEYUP2].forEach(function(keyMap, index) {
-    Object.keys(keyMap).forEach(function(keyname) {
-      scene.input.keyboard.on('keyup_' + keyname, function (event) {
-        let id = game.fingerprint + 'player' + (index + 1);
+  Object.keys(KEYDOWN).forEach(function(keyname) {
+    scene.input.keyboard.on('keyup_' + keyname, function (event) {
+      let id = game.fingerprint;
 
-        if (Player.activePlayers[id]) {
-          command = keyMap[keyname]
-          echo_command(id, command);
-        }
-      });
+      if (Player.activePlayers[id]) {
+        command = KEYUP[keyname]
+        echo_command(id, command);
+      }
     });
   });
 };
