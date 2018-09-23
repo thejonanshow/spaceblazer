bd0dcbac503c1fa6ea22e8412e724e55d7b67967
Jonan Scheffler <jonanscheffler@gmail.com>
2018-09-23 11:38:22 -0700

    Passport commands add ips to allowed hosts.

--- config/initializers/commands.rb
+++ config/initializers/commands.rb
@@ -1,4 +1,10 @@
+Thread.report_on_exception = true
 last_message = nil
+
+def add_ip(ip)
+  existing = [Rails.configuration.action_cable.allowed_request_origins].flatten
+  Rails.configuration.action_cable.allowed_request_origins = (existing + [ip]).flatten
+end
 
 Thread.new do
   redis = Redis.new(url: ENV['REDIS_URL'])
@@ -7,7 +13,11 @@ Thread.new do
     on.message do |channel, message|
       Rails.logger.debug { message } if ENV["LOG_COMMANDS"]
 
-      if message == last_message
+      if (message.match /My voice is my passport, verify me/)
+        ip = message.split(':').last.strip
+        add_ip(ip)
+        Rails.logger.debug("#{ip} added to action cable allowed request origins.")
+      elsif message == last_message
         Rails.logger.debug { "Duplicate message received" } if ENV["LOG_COMMANDS"]
       else
         Rails.logger.debug { "Not a duplicate: #{message} != #{last_message}" } if ENV["LOG_COMMANDS"]
