5e86997d37c87e39def9aa5cf41e1fadef1ea9fa
Jonan Scheffler <jonanscheffler@gmail.com>
2018-09-23 21:01:21 -0700

    Model bankruptcy.

--- db/migrate/20180917190830_add_index_to_player.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexToPlayer < ActiveRecord::Migration[5.2]
-  def change
-    add_index :players, [:client_side_id, :game_id], unique: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,33 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_23_191206) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "games", force: :cascade do |t|
-    t.boolean "active", default: false, null: false
-    t.jsonb "data", default: "{}", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "laserbonnets", force: :cascade do |t|
-    t.string "pi_id"
-    t.boolean "online"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "players", force: :cascade do |t|
-    t.string "client_side_id"
-    t.string "avatar"
-    t.string "color"
-    t.integer "game_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["client_side_id", "game_id"], name: "index_players_on_client_side_id_and_game_id", unique: true
-  end
 
 end
