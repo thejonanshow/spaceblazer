8f19a085330a9159000dcd6329db762e02e34fbf
Jonan Scheffler <jonanscheffler@gmail.com>
2018-09-23 21:18:49 -0700

    Use UUID primary keys.

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,10 @@ module Spaceblazer
     config.assets.paths << Rails.root.join('node_modules')
     config.active_job.queue_adapter = :sidekiq
 
+    config.generators do |g|
+      g.orm :active_record, primary_key_type: :uuid
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
